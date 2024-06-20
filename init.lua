1e91d19fc14664dcb88b20569525d5a168516026
Yannick Spies <yannickspies91@gmail.com>
2024-06-20 11:36:29 +0200

    added dracula theme

--- init.lua
+++ init.lua
@@ -820,24 +820,85 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  {
+    'maxmx03/dracula.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      ---@type dracula
+      local dracula = require 'dracula'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      dracula.setup {
+        transparent = false,
+        on_colors = function(colors, color)
+          ---@type dracula.palette
+          return {
+            -- override or create new colors
+            mycolor = '#ffffff',
+          }
+        end,
+        on_highlights = function(colors, color)
+          ---@type dracula.highlights
+          return {
+            ---@type vim.api.keyset.highlight
+            Normal = { fg = colors.mycolor },
+          }
+        end,
+        plugins = {
+          ['nvim-treesitter'] = true,
+          ['nvim-lspconfig'] = true,
+          ['nvim-navic'] = true,
+          ['nvim-cmp'] = true,
+          ['indent-blankline.nvim'] = true,
+          ['neo-tree.nvim'] = true,
+          ['nvim-tree.lua'] = true,
+          ['which-key.nvim'] = true,
+          ['dashboard-nvim'] = true,
+          ['gitsigns.nvim'] = true,
+          ['neogit'] = true,
+          ['todo-comments.nvim'] = true,
+          ['lazy.nvim'] = true,
+          ['telescope.nvim'] = true,
+          ['noice.nvim'] = true,
+          ['hop.nvim'] = true,
+          ['mini.statusline'] = true,
+          ['mini.tabline'] = true,
+          ['mini.starter'] = true,
+          ['mini.cursorword'] = true,
+        },
+      }
+      vim.cmd.colorscheme 'dracula'
+      vim.cmd.colorscheme 'dracula-soft'
     end,
   },
-
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        theme = vim.g.colors_name,
+        refresh = {
+          statusline = 1000,
+        },
+      },
+    },
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
